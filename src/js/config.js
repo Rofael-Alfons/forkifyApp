9a88974d81545bb23ed57f378117d5b3b9aee169
Rofael-Alfons <rofa.alfons7@gmail.com>
2023-02-13 00:35:51 +0200

    intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 5;
+export const KEY = 'baa0da05-0d44-4f5b-a14a-b9ed38a3a76d';
+export const MODAL_CLOSE_SEC = 2.5;
